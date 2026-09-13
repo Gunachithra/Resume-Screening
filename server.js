3bea95356b19487485a77c8d751d1bad7adea34c
Gunachithra <chithra25122004@gmail.com>
2026-09-13 13:41:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname));
 
 // MongoDB Connection
 mongoose.connect("mongodb://127.0.0.1:27017/resumeDB")
